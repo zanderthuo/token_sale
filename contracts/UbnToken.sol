b76a5a9b411e9382a8a00e67e78cc6103f55dde5
Alexander <thuozander@gmail.com>
2018-11-19 11:33:04 +0300

    3. Delegated Transfer

--- contracts/UbnToken.sol
+++ contracts/UbnToken.sol
@@ -14,16 +14,25 @@ contract UbnToken {
 	  		uint256 _value
 	  	);
 
-	  mapping (address => uint256) public balanceOf;
-	  
+	  //approve
+	  event Approval(
+	  	  address indexed _owner,
+	  	  address indexed _spender,
+	  	  uint256 _value
+	  	);
 
+	  mapping (address => uint256) public balanceOf;
+	  mapping (address => mapping(address => uint256)) public allowance;
+	  
+	  //allowance
+
+	  
 	  constructor(uint256 _initialSupply) public {
 	  	balanceOf[msg.sender] = _initialSupply;
 	  	totalSupply = _initialSupply;
 	  
 	  }
 
-	  
 	  function transfer (address _to, uint256 _value) public returns(bool success) {
 	  
 	  	require(balanceOf[msg.sender] >= _value);
@@ -37,5 +46,37 @@ contract UbnToken {
 
 	    return true;
 	  }
-	  
+
+	  //Delegated Transfer 
+
+	  //approve
+	  function approve(address _spender, uint256 _value) public returns(bool success) {
+         //allowance
+         allowance[msg.sender][_spender] = _value;
+
+	  	//Approval event
+	  emit Approval(msg.sender, _spender, _value);
+
+	  	return true;
+	  }
+	  //transferFrom
+	  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+	  		require(_value <= balanceOf[_from]);
+	  		require(_value <= allowance[_from][msg.sender]);
+	  	//Require _fom has enough tokens
+	  	//Require allowance is big enough
+	  	//Change the balance
+	  	balanceOf[_from] -= _value;
+	  	balanceOf[_to] += _value;
+	  	
+	  	//Update the allowance
+	  	allowance[_from][msg.sender] -= _value;
+
+	  	//Transfer event
+	  	emit Transfer(_from, _to, _value);
+	  	//return a boolean
+
+	  	return true;
+
+	  }
 }
